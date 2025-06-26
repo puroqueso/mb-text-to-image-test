28785eb2a2329e0c8f2b484542ad0faae47ca975
puroqueso <mbhc97@hotmail.com>
2025-06-26 07:17:12 -0500

    Update index.ts
    Update prompt from "marvel characters" to "videogames"

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "marvel characters",
+      prompt: "videogames",
     };
 
     const response = await env.AI.run(
